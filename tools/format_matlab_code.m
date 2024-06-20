9d329ca0d6e55a865ca2939a27d8e03d9c97fe66
latercomer <latercomer@qq.com>
2024-06-21 00:29:43 +0800

    添加docs文件夹

---
+++ tools/format_matlab_code.m
@@ -0,0 +1,13 @@
+cwd = fileparts(mfilename('fullpath')+".m");
+root = fullfile(cwd, '../');
+mbeautifier_dir = fullfile(root, 'code', 'MBeautifier');
+
+addpath(mbeautifier_dir)
+list = dir(fullfile(root, '**', '*.m'));
+for i = 1:length(list)
+    if ~contains(list(i).folder, 'MBeautifier')
+        mfile = fullfile(list(i).folder, list(i).name);
+        MBeautify.formatFile(mfile, mfile);
+    end
+end
+rmpath(mbeautifier_dir)
