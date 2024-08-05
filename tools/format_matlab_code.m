4936c1cd4da151e59fae855c53d434c01683b36b
latercomer <latercomer@qq.com>
2024-08-05 09:13:57 +0800

    移动MBeautifier到tools文件夹

--- tools/format_matlab_code.m
+++ tools/format_matlab_code.m
@@ -1,8 +1,9 @@
 cwd = fileparts(mfilename('fullpath')+".m");
-root = fullfile(cwd, '../');
-mbeautifier_dir = fullfile(root, 'code', 'MBeautifier');
-
+mbeautifier_dir = fullfile(root,  'MBeautifier');
 addpath(mbeautifier_dir)
+
+% format m-file in code folder
+root = fullfile(cwd, '../');
 list = dir(fullfile(root, '**', '*.m'));
 for i = 1:length(list)
     if ~contains(list(i).folder, 'MBeautifier')
@@ -10,4 +11,5 @@ for i = 1:length(list)
         MBeautify.formatFile(mfile, mfile);
     end
 end
+
 rmpath(mbeautifier_dir)
